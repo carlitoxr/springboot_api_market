e21a48ec08e40b6dbfa4942c5cca5e5301099f2c
carlitoxr <carlosr9105@gmail.com>
2023-12-11 11:33:55 -0300

    Se agrega @Service

--- src/main/java/com/market/market/persistence/ProductoRepository.java
+++ src/main/java/com/market/market/persistence/ProductoRepository.java
@@ -5,6 +5,7 @@ import com.market.market.domain.repository.ProductRepository;
 import com.market.market.persistence.crud.ProductoCrudRepository;
 import com.market.market.persistence.entity.Producto;
 import com.market.market.persistence.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,7 +13,9 @@ import java.util.Optional;
 
 @Repository
 public class ProductoRepository implements ProductRepository {
+    @Autowired
     private ProductoCrudRepository productoCrudRepository;
+    @Autowired
     private ProductMapper mapper;
 
     @Override
