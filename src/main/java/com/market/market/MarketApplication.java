ff7e73afc164d86244937ad39768aeaa5c70be6b
carlitoxr <carlosr9105@gmail.com>
2023-12-11 12:48:47 -0300

    Se agrega ProductController y se modifica la anotación de la clase MarketApplication (scanBasePackages={com.market.market.persistence.mapper}) quedando @SpringBootApplication(scanBasePackages={com.market.market.persistence.mapper})

--- src/main/java/com/market/market/MarketApplication.java
+++ src/main/java/com/market/market/MarketApplication.java
@@ -3,7 +3,7 @@ package com.market.market;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"com.market.market.persistence.mapper"})
 public class MarketApplication {
 
 	public static void main(String[] args) {
